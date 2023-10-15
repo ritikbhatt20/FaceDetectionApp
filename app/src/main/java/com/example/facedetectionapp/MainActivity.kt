df4217eaa94e8e0cd3fdd22ee40c0b76ae98e6e3
ritik <ritikbhatt020@gmail.com>
2023-10-15 16:03:30 +0530

    Detection of image done

--- app/src/main/java/com/example/facedetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/facedetectionapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.facedetectionapp
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCamera = findViewById<Button>(R.id.btnCamera)
+
+        buttonCamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(intent.resolveActivity(packageManager) != null){
+                startActivityForResult(intent, 123)
+            }
+            else{
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==123 && resultCode== RESULT_OK){
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                detectFace(bitmap)
+            }
+        }
+    }
+
+    private fun detectFace(bitmap: Bitmap){
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        val detector = FaceDetection.getClient(options)
+
+        val image = InputImage.fromBitmap(bitmap, 0)
+
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                // Task completed successfully, our face is successfully detected
+
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+
+            }
+
     }
 }
